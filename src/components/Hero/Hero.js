f17ac6a771b4a11cea034ff5cb9a3cd6633330ac
jclevecodes <joshua.cleveland02@gmail.com>
2025-03-02 23:57:37 -0500

    hero page update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,23 @@
+// src/components/Hero.jsx
 import React from 'react';
 import useInView from '../../hooks/useInView';
 import './Hero.css';
+import profilePic from '../../images/profilev2.png';
 
 const Hero = () => {
   const ref = useInView();
+
   return (
     <section id="home" className="hero fade-up" ref={ref}>
-      <div className="hero-content">
-        <h1>Joshua Cleveland</h1>
-        <p>Coding the future one line at a time.</p>
+      <div className="hero-container">
+        <div className="hero-text">
+          <h1>Hello, I'm</h1>
+          <h1>Joshua Cleveland</h1>
+          <p>Coding the future one line at a time.</p>
+        </div>
+        <div className="hero-image">
+          <img src={profilePic} alt="Professional Headshot" />
+        </div>
       </div>
     </section>
   );
